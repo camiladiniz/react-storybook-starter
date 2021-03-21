aa4dbc6a94438d2148d887c913e1283f7fe5aea2
cdiniz <cdiniz@eusou.space>
2021-03-21 02:15:06 -0300

    buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './Button.css';
 
 
-const Button = ({ children, ...rest }) => {
+const Button = ({ children, disabled = false, ...rest }) => {
   return (
-    <button className="button" {...rest}>
+    <button className={`button ${disabled ? 'disabled' : ''}`} {...rest} disabled={disabled}>
       { children }
     </button>
   )
 }
 
 export default Button;
+
+Button.propTypes = {
+  disabled: PropTypes.bool
+}
+
+Button.defaultProps = {
+  disabled: false
+}
--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -1,6 +1,7 @@
 // Button.stories.js
 
 import React from 'react';
+
 import { action } from '@storybook/addon-actions';
 
 import Button  from './Button';
@@ -8,12 +9,21 @@ import Button  from './Button';
 export default {
   title: 'Components/Button',
   component: Button,
+  decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
 }
+
+const Template = args => <Button {...args}></Button>
+
+export const ButtonPrimary = Template.bind({});
 
 export const Primary = () => <Button primary onClick={action('clicked')}>Button</Button>;
 Primary.storyName = 'I am the primary';
 
-const Template = args => <Button {...args} />;
+
+export const Disabled = () => <Button primary onClick={action('clicked')} disabled={true}>Button</Button>;
+Disabled.args = {
+    disabled:true
+}
 
 export const Emoji = () => (
     <Button primary onClick={action('clicked')}>
