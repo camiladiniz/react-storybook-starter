acaf0008e4771c9abf386be387437c342dce01d8
cdiniz <cdiniz@eusou.space>
2021-03-20 23:40:07 -0300

    screens

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Task from './Task';
 
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
 export default {
     component: Task,
     title: 'Task'
@@ -13,7 +14,7 @@ export const Default = Template.bind({});
 Default.args = {
     task: {
         id: '1',
-        title: 'Test Task',
+        title: longTitleString,
         state: 'TASK_INBOX',
         updatedAt: new Date(2018, 0, 1, 9, 0)
     },
